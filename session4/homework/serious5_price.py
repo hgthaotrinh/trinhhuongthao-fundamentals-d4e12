88c857fe095e0d73a3eee0310f6340c585fa991c
Thao Trinh <junerosie@gmail.com>
2020-05-25 14:21:39 +0700

    update hw session4

--- session4/homework/serious5_price.py
+++ session4/homework/serious5_price.py
@@ -12,4 +12,24 @@ stocks = {
     "pear": 15
 }
 
-for i in 
+for key in prices: 
+    price = prices[key]
+    stock = stocks[key]
+    print(key)
+    print('price:', price) 
+    print('stock:', stock)
+    print()
+
+total = 0
+for key in prices: 
+    total += prices[key] * stocks[key]
+print('total revenue: $', total)
+
+
+#check:
+banana = 4 * 6
+apple = 2 * 0 
+orange = 1.5 * 32
+pear = 3 * 15
+revenue = banana + apple + orange + pear
+print('total check', revenue)
